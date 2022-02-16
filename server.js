82b11ed762191d526f3a9ef1d88a5d797bb91a96
MELAPU . PARUSURAM NAIDU <69456055+mn7533@users.noreply.github.com>
2022-02-16 08:18:40 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+// app.use(cors())
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+const { v4: uuidV4 } = require('uuid')
